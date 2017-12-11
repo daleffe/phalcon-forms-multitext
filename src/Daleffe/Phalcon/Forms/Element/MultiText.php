b6c1008e3c7784015c4ae7b1879a3ae95c6df5ba
Guilherme Roberge Daleffe <falecomdaleffe@gmail.com>
2017-12-11 16:48:43 -0200

    Adjust compoennt to see attributes

--- src/Daleffe/Phalcon/Forms/Element/MultiText.php
+++ src/Daleffe/Phalcon/Forms/Element/MultiText.php
@@ -5,11 +5,7 @@ namespace Daleffe\Phalcon\Forms\Element;
 class MultiText extends \Phalcon\Forms\Element
 {
     public function render($attributes = [])
-    {
-        var_dump($attributes);
-        die();
-        
-        /*$checked = null;
+    {   
         $html = '';
 
         if (is_array($attributes)) {
@@ -20,15 +16,18 @@ class MultiText extends \Phalcon\Forms\Element
             $attributes = $this->getAttributes();
         }
 
-        foreach ($attributes['elements'] as $key => $value) {
+        var_dump($attributes);
+        die();
+
+        /*foreach ($attributes['elements'] as $key => $value) {
             $checked = ($key == $this->getValue()) ? ' checked' : null;
 
             $html .= '<div class="' . $attributes['class'][$key] . '">';
             $html .= '<input type="radio" id="' . $this->getName() . $key . '" name="' . $this->getName() . '" value="' . $key . '"' . $checked . ' />';
             $html .= '<label for="' . $this->getName() . $key . '">' . $value . '</label>';
             $html .= "</div>";
-        }
+        }*/
 
-        return $html;*/
+        return $html;
     }
 }
