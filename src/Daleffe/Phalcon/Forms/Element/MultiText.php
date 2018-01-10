7de4ce1bf03bf3eadcd1c526ef078fe8d9183acd
Guilherme Roberge Daleffe <falecomdaleffe@gmail.com>
2018-01-10 20:25:06 -0200

    Added missing quotation marks of class from input type

--- src/Daleffe/Phalcon/Forms/Element/MultiText.php
+++ src/Daleffe/Phalcon/Forms/Element/MultiText.php
@@ -21,7 +21,7 @@ class MultiText extends \Phalcon\Forms\Element
             if (is_array($default)) $default = $default[0];
 
             $html .= '<div class="control-group input-group ' . $attributes['div-class'] .'" style="margin-top:10px">';
-            $html .= '<input type="text" name="' . $this->getName() . '" class=' . $attributes['class'] . ' placeholder="' . $attributes['placeholder'] . '" value="' . $default . '" />';
+            $html .= '<input type="text" name="' . $this->getName() . '" class="' . $attributes['class'] . '" placeholder="' . $attributes['placeholder'] . '" value="' . $default . '" />';
             $html .= '<div class="input-group-btn"> ';
             $html .= '<button class="btn btn-basic ' . $attributes['button-class'] . '" type="button"><i class="glyphicon glyphicon-remove"></i></button>';
             $html .= '</div>';
